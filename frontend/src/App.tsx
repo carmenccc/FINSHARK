af82f4fff90b07eb9ca0653bd65310ab1a8e213b
Carmencc <carmencai5114@gmail.com>
2025-01-09 12:12:25 -0500

    Portfolio CRUD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,16 @@ import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
 import { CompanySearch } from "./company.e";
 import { searchCompanies } from "./api";
+import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
 
 function App() {
   // Define state
   const [search, setSearch] = useState<string>("");
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
 
+  // Event Handlers
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
@@ -29,6 +32,16 @@ function App() {
     }
   };
 
+  const onPortfolioCreate = (e: any) => {
+    e.preventDefault();
+    // Ensure no duplicate add
+    const exists = portfolioValues.find((value) => value === e.target[0].value);
+    if (exists) return;
+    // Add to portfolio
+    setPortfolioValues([...portfolioValues, e.target[0].value]);
+  };
+
+  // Return JSX
   return (
     <div className="App">
       <Search
@@ -36,8 +49,12 @@ function App() {
         handleSearchChange={handleSearchChange}
         onSearchSubmit={onSearchSubmit}
       />
+      <ListPortfolio portfolioValues={portfolioValues} />
+      <CardList
+        searchResults={searchResult}
+        onPortfolioCreate={onPortfolioCreate}
+      />
       {serverError && <div>unable to connecct to API</div>}
-      <CardList searchResults={searchResult} />
     </div>
   );
 }
