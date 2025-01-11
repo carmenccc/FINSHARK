0fc44bb6f78349fac98002b5daaf415a5390cadf
Carmencc <carmencai5114@gmail.com>
2025-01-11 10:45:19 -0500

    Comment CRUD

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Comment;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -13,9 +14,11 @@ namespace api.Controllers
     public class CommentController : ControllerBase
     {
         private readonly ICommentRepository _commentRepo;
-        public CommentController(ICommentRepository commentRepo)
+         private readonly IStockRepository _stockRepo;
+        public CommentController(ICommentRepository commentRepo, IStockRepository stockRepo)
         {
             _commentRepo = commentRepo;
+            _stockRepo = stockRepo;
         }
 
         [HttpGet]
@@ -24,6 +27,52 @@ namespace api.Controllers
             var commentDtos = comments.Select(c => c.ToCommentDto());
 
             return Ok(commentDtos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id){
+            var comment = await _commentRepo.GetByIdAsync(id);
+            if(comment == null){
+                return NotFound();
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> Create([FromRoute] int stockId, [FromBody] CreateCommentDto commentDto){
+           if(!await _stockRepo.StockExists(stockId)){
+            return BadRequest("Stock does not exist");
+           }
+
+           var newComment = commentDto.ToCommentFromCreate(stockId);
+           await _commentRepo.CreateAsync(newComment);
+
+            return CreatedAtAction(nameof(GetById), new {id = newComment.Id}, newComment.ToCommentDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto commentDto){
+           var comment = await _commentRepo.UpdateAsync(id, commentDto.ToCommentFromUpdate());
+           
+           if(comment == null){
+            return BadRequest("Comment does not exist");
+           }
+
+            return Ok(comment.ToCommentDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id){
+           var comment = await _commentRepo.DeleteAsync(id);
+           
+           if(comment == null){
+            return NotFound("Comment does not exist");
+           }
+
+            return Ok(comment);
         }
     }
 }
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a1c7c0c649c1530ed1ed53d0b65dd282f4ce77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d54b85698f2e11ca42765fba58cfd2dee4d70db")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
