f45af44d943a5d75a5014905ce416b681e630e28
Carmencc <carmencai5114@gmail.com>
2025-01-10 21:45:19 -0500

    Configure Swagger UI

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,13 @@
 using api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -12,12 +16,20 @@ builder.Services.AddDbContext<ApplicationDBContext>(options => {
 // Control dependency injection
 var app = builder.Build();
 
+app.MapOpenApi();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-   app.MapOpenApi();
+   app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+   });
+
+   
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+953004fda3c0a30516dae2e4fa52faf47a1c85d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c9fca29b3a10009aecd0c0c9b03f86aadb97dd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
