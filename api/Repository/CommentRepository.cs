dd259410f90ec9a1a7b598e799ddfd51045a6038
Carmencc <carmencai5114@gmail.com>
2025-01-12 21:27:54 -0500

    Comment search filter & sort

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Helpers;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -39,9 +40,22 @@ namespace api.Repository
             return commentModel;
         }
 
-        public async Task<List<Comment>> GetAllAsync()
+        public async Task<List<Comment>> GetAllAsync(CommentQueryObject queryObject)
         {
-            return await _context.Comments.Include(c => c.AppUser).ToListAsync();
+            var comments =  _context.Comments.Include(c => c.AppUser).AsQueryable();
+
+            // Filter
+            if(!string.IsNullOrWhiteSpace(queryObject.Symbol))
+            {
+                comments = comments.Where(c => c.Stock.Symbol == queryObject.Symbol);
+            };
+            // Sort
+            if(queryObject.IsDecsending == true)
+            {
+                comments = comments.OrderByDescending(c => c.CreatedOn);
+            };
+
+            return await comments.ToListAsync();
         }
 
         public async Task<Comment?> GetByIdAsync(int id)
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8ac4c3ad9d2d685b04fe945275c6ef41d3d75f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a984ca5e4bcda33d7b08b75a9559beab51a5e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
