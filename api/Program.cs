4a984ca5e4bcda33d7b08b75a9559beab51a5e9d
Carmencc <carmencai5114@gmail.com>
2025-01-12 21:13:46 -0500

    Config Cors

--- api/Program.cs
+++ api/Program.cs
@@ -89,6 +89,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    //.WithOrigins("https://localhost:44351) for deployment
+    .SetIsOriginAllowed(origin => true));
+
 app.UseAuthentication();
 app.UseAuthorization();
 
