fcf13e573a14a4fda93ed1d7783305eff2816fd6
Carmencc <carmencai5114@gmail.com>
2025-01-12 23:28:11 -0500

    UserContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
-      <ToastContainer />
+      <UserProvider>
+        <Navbar />
+        <Outlet />
+        <ToastContainer />
+      </UserProvider>
     </>
   );
 }
